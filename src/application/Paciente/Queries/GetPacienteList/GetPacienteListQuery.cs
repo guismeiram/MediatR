42997f9c7568fa2ea0251b2026cc72b5c0f2846e
Guilherme Mendes <boralagui@outlook.com>
2024-01-05 21:46:14 -0300

    List Query

---
+++ src/application/Paciente/Queries/GetPacienteList/GetPacienteListQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace application.Paciente.Queries.GetPacienteList
+{
+    internal class GetPacienteListQuery
+    {
+    }
+}

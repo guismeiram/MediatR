85c812edff16a908e37b0fe887aed02ecd1b6742
Guilherme Mendes <boralagui@outlook.com>
2024-01-06 00:08:09 -0300

    GetListQuery

--- src/application/Paciente/Queries/GetPacienteList/GetPacienteListQuery.cs
+++ src/application/Paciente/Queries/GetPacienteList/GetPacienteListQuery.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using application.Common.Interfaces;
+using application.Common.Models;
+using AutoMapper;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace application.Paciente.Queries.GetPacienteList
 {
-    internal class GetPacienteListQuery
+    public class GetPacienteListQuery
     {
+        public class Query : IRequest<Result<PacienteDto>>
+        {
+            public Guid Id { get; set; }
+
+        }
+
+        public class Handler : IRequestHandler<Query, Result<PacienteDto>>
+        {
+            private readonly IUnitOfWork _uow;
+            private readonly IMapper _mapper;
+
+            public Handler(IUnitOfWork uow, IMapper mapper)
+            {
+                _uow = uow;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<PacienteDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                //var paciente = await _uow.PacienteRepository
+                    
+
+                return Result<PacienteDto>.Success();
+            }
+        }
     }
 }
--- src/application/Paciente/Queries/GetPacienteList/PacienteDto.cs
+++ src/application/Paciente/Queries/GetPacienteList/PacienteDto.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using application.Common.Mappings;
+using application.Paciente.Command.UpdatePaciente;
+using AutoMapper;
+using domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,19 @@ using System.Threading.Tasks;
 
 namespace application.Paciente.Queries.GetPacienteList
 {
-    internal class PacienteDto
+    public class PacienteDto : IMapFrom<domain.Entities.Paciente>
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+        public string Rg { get; set; }
+        public Sexo Sexo { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdatePacienteDto, domain.Entities.Paciente>().ReverseMap()
+                                .ForMember(x => x.Sexo, opt => opt.MapFrom(src => src.Sexo));
+
+        }
     }
 }
